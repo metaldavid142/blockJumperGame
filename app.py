efccf5a0efec01b4ec037e5f646a5216829becb2
metaldavid <mdsangucho@espe.edu.ec>
2022-06-28 22:01:53 -0500

    Docstring colocados en todos los archivos

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+#Importar librerías
 import os
-
 from flask import Flask, render_template
 
+
 app=Flask(__name__)
+#Ruta hacia la carpeta tempaltes
 app._static_folder = os.path.abspath("templates/static/")
 @app.route("/")
 def game():
+    """
+    Llama a la página donde se presenta el juego
+
+    Returns
+    -------
+    Retorna la página game.html alojada en la carpeta layouts 
+    """  
     return render_template("layouts/game.html")
 
 if __name__=='__main__':
--- templates/static/js/game.js
+++ templates/static/js/game.js
@@ -1,7 +1,9 @@
+// Inicio del juego
 window.addEventListener('load', ()=>{
   startGame()
 })
 
+// Inicialización de variables
 var canvasWidth = 600;
 var canvasHeight = 400;
 
@@ -16,29 +18,61 @@ var jumpSpeed = 0;
 
 var block;
 
-// Create a score of 0 to start with
+// Inicialización del puntaje en cero
 var score = 0;
-// Create a variable to hold our scoreLabel
+// Creación de la variable para guardar el puntaje
 var scoreLabel;
 
 function startGame() {
+  /*
+  Inicio del juego Block Jumper
+        
+  Variables
+  ----------
+  player :
+    creación de un nuevo jugador
+  block :
+    creación del bloque
+  scoreLabel :
+    asignación a la variable puntaje  el nuevo valor de puntaje
+  */
   initCanvas();
   player = new createPlayer(30, 30, 10);
   block = new createBlock();
-  // Assign your scoreLabel variable a value from scoreLabel()
   scoreLabel = new createScoreLabel(10, 30);
 }
 
-let context; /* La creamos para guardar nuestro contexto al iniciar el canvas */
+//Se crea para guardar el contexto al iniciar el canvas
+let context; 
 
 function initCanvas() {
-  let canvas = document.querySelector("#canvas")  /* Seleccionamos nuestro canvas */ 
-  canvas.width = canvasWidth; /* Le pasamos el ancho y el alto */
+  /*
+  Inicio de canvas
+        
+  Variables
+  ----------
+  let canvas :
+    seleccionar el canvas
+  canvas.width, canvas.height :
+    ancho y alto de canvas
+  */
+  let canvas = document.querySelector("#canvas")
+  canvas.width = canvasWidth;
   canvas.height = canvasHeight;
   context = canvas.getContext("2d");    
 }
 
 function createPlayer(width, height, x) {
+  /*
+  Creación de la caja jugador recibiendo como parámetro el ancho y alto
+        
+  Funciones
+  ----------
+  draw : para dibujar la caja del jugador, ancho y alto
+  makeFall : velocidad de la caída de la caja
+  stopPlayer : para detener el juego en caso de colisión
+  jump : velocidad del salto de la caja
+  */
   this.width = width;
   this.height = height;
   this.x = x;
@@ -46,7 +80,7 @@ function createPlayer(width, height, x) {
   
   this.draw = function() {
       ctx = context;
-      ctx.fillStyle = "green";
+      ctx.fillStyle = "blue";
       ctx.fillRect(this.x, this.y, this.width, this.height);
   }
   this.makeFall = function() {
@@ -65,12 +99,21 @@ function createPlayer(width, height, x) {
   this.jump = function() {
       if (isJumping) {
           this.y -= jumpSpeed;
-          jumpSpeed += 0.3;
+          jumpSpeed += 0.6;
       }
   }
 }
 
 function createBlock() {
+  /*
+  Creación del bloque obstáculo a superar, variables de ancho, alto y velocidad con valores aleatorios
+        
+  Funciones
+  ----------
+  draw : para dibujar el bloque obstáculo
+  attackPlayer : atacar al jugador
+  returnToAttackPosition : para reiniciar su partida inicial
+  */
   var width = randomNumber(10, 50);
   var height = randomNumber(10, 200);
   var speed = randomNumber(2, 6);
@@ -94,13 +137,16 @@ function createBlock() {
           speed = randomNumber(4, 6);
           this.y = canvasHeight - height;
           this.x = canvasWidth;
-          // Increase your score if your block made it to the edge
+          // Incrementa el puntaje en caso de superar el bolque obstáculo
           score++;
       }
   }
 }
 /* Detectar cuando se choca */
 function detectCollision() {
+  /*
+  Detecta la colisión del bloque con el jugador a través de igualar las variables
+  */
   var playerLeft = player.x
   var playerRight = player.x + player.width;
   var blockLeft = block.x;
@@ -109,15 +155,18 @@ function detectCollision() {
   var playerBottom = player.y + player.height;
   var blockTop = block.y;
   
+  // Detiene el juego al colapasar los bloques y devuelve una alerta de fin de juego
   if (playerRight > blockLeft && 
       playerLeft < blockLeft && 
       playerBottom > blockTop) {
-      alert("Fin del juego") /* Detiene el juego al chocar */
+      alert("Fin del juego")
   }
 }
 
-/* Crear el tablero del puntaje */
 function createScoreLabel(x, y) {
+  /*
+  Creación del tablero del puntaje
+  */
   this.score = 0;  
   this.x = x;
   this.y = y;
@@ -130,6 +179,21 @@ function createScoreLabel(x, y) {
 }
 
 function updateCanvas() {
+  /*
+  Actualización del canvas al reiniciar el juego
+        
+  Funciones
+  ----------
+  Llama a las funciones del jugador:
+    detectCollision
+    makeFall
+    draw
+    jump
+
+  Llama a las funciones del bloque obstáculo:
+    draw
+    attackPlayer
+  */
   detectCollision();
   
   ctx = context;
@@ -142,23 +206,26 @@ function updateCanvas() {
   block.draw();
   block.attackPlayer();
   
-  // Redraw your score and update the value
+  // Reinicia el puntaje y actualiza el valor
   scoreLabel.text = "SCORE: " + score;
   scoreLabel.draw();
 }
 
+// Función que genera valores aleatorios
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+// Función para reiniciar los saltos
 function resetJump() {
   jumpSpeed = 0;
   isJumping = false;
 }
 
+// Define la altura del salto del bloque jugador
 document.body.onkeyup = function(e) {
   if (e.keyCode == 32) {
       isJumping = true;
-      setTimeout(function() { resetJump(); }, 1000);
+      setTimeout(function() { resetJump(); }, 900);
   }
 }
